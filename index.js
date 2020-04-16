1e9028c36f5ebfffead5829e2852c8d77eac76e9
Oliver Harold <oliverhsj@hotmail.dk>
2020-04-16 13:40:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.static(express.static('public'));
+app.use(express.static('public'));
 
 
 app.listen(port);
