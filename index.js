4a812edd83131e05c1fc43e632ff60ee5c3c20a7
Oliver Harold <oliverhsj@hotmail.dk>
2020-04-16 13:57:53 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
 const express = require('express');
 const app = express();
+var http = require('http').createServer(app);
 
 var io = require('socket.io')(http);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 1337;
 
 app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.emit('chat message', "SOCKET.IO IS WORKING");
+});
 
 app.listen(port);
