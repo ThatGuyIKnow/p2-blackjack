147877eab6e82a67a002e4111e1a34ad7f8611dc
Oliver Harold <oliverhsj@hotmail.dk>
2020-04-16 13:15:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 1337
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => {
+  res.type('text/plain')
+  res.send('Hello World!')
+})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
