2e3b12c5e625a16f6c021d425945d812b53919cf
Oliver Harold <oliverhsj@hotmail.dk>
2020-04-17 14:28:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,11 @@ io.on('connection', (socket) => {
       Object.keys(socket.rooms).map((room) => room != socket.id ? socket.leave(room) : socket.id);
     }
     */
-      console.log(rooms.includes(roomID));
-      socket.emit('chat message', roomID + ' ' + rooms.includes(roomID));
-      socket.join(roomID, (err) => {
-        socket.emit('chat message', `Joined room ${roomID}`);
+   if(rooms.includes(roomID)){
+      socket.join(roomID, () => {
+        socket.emit('chat message', `Rooms ${Object.keys(socket.rooms)}`);
       });
-    
+    }
   });
   socket.on('disconnect', (socket) => {
     console.log("disconnected")
