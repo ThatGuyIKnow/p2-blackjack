a7b5eccb33752d940b8a23bb80f31e454e052833
Oliver Harold <oliverhsj@hotmail.dk>
2020-04-17 12:56:22 +0200

    Testing room functionality Socket IO
    Testing function for exclusively joining a single room at the time (ignoring the default socket ID room)

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
+const rooms = ['room1', 'room2', 'room3', 'room4'];
+
 io.on('connection', (socket) => {
-  socket.join('boycott_nestle');
-  io.to('boycott_nestle')
-    .emit('chat message', 
-          'This is a message for the room: A player has joined');
-  socket.emit('chat message', "This is a message for the player");
+  socket.emit('chat message', typeof socket.rooms + ' ' + socket.rooms);
+  socket.on('access_room', (roomID) => {
+    if(socket.rooms != [socket.id]) {
+      socket.rooms.map((room) => room != socket.id ? socket.leave(room) : socket.id);
+    }
+    if(rooms.include(roomID)) {
+      socket.join(roomID);
+    }
+  });
   socket.on('disconnect', (socket) => {
     console.log("disconnected")
   })
