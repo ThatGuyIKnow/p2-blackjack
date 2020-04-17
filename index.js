453ccc8c4f1e73b5510e24de68891294db564794
Oliver Harold <oliverhsj@hotmail.dk>
2020-04-17 14:31:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ app.use(express.static('public'));
 const rooms = ['room1', 'room2', 'room3', 'room4'];
 
 io.on('connection', (socket) => {
-  socket.join('room5', (err) => socket.emit('chat message', 'Joined Room5'));
   socket.emit('chat message', `Connected through WebSocket`);
   socket.on('access_room', (roomID) => {
-    if(Object.keys(socket.rooms) != [socket.id]) {
-      socket.emit('chat message', '3' + roomID);
-      Object.keys(socket.rooms).map((room) => room != socket.id ? socket.leave(room) : socket.id);
-    }
+    
+    Object.keys(socket.rooms).map((room) => 
+      room != socket.id ? socket.leave(room) : socket.id);
+    
    if(rooms.includes(roomID)){
       socket.join(roomID, () => {
         socket.emit('chat message', `Rooms ${Object.keys(socket.rooms)}`);
