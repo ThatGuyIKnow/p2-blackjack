b95513cc98c2d3690ec568c2f8196dbcac5bd0c2
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2020-04-27 12:41:05 +0200

    Made stuff pretty

--- index.js
+++ index.js
@@ -8,42 +8,39 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 
 const options = {
-  pingTimeout : 60000, //1 minute(s)
+  pingTimeout: 60000, //1 minute(s)
 }
 
 const rooms = {
-  'room1': 
-  {
-    connections : [],
-    maxConnections : 1
-  }, 
-  'room2': 
-  {
-    connections : [],
-    maxConnections : 1
-  }, 
-  'room3': 
-  {
-    connections : [],
-    maxConnections : 1
-  }, 
-  'room4': 
-  {
-    connections : [],
-    maxConnections : 1
-  }, 
+  'room1': {
+    connections: [],
+    maxConnections: 1,
+    state: {}
+  },
+  'room2': {
+    connections: [],
+    maxConnections: 1
+  },
+  'room3': {
+    connections: [],
+    maxConnections: 1
+  },
+  'room4': {
+    connections: [],
+    maxConnections: 1
+  },
 };
 
 // @param {string} data The cookie string
 function parseCookie(data) 
 {
   let parseData =  [...data.matchAll(/([^=\s]*)=([^;\s]*)/gm)];
- 
   /* Converts the parsed data to an object with the key-value pairs found in    
+  /* Converts the parsed data to an object with the key-value pairs found in
    * the cookie (match[0] is the entire cookie string).
    */
   let cookie = {};
-  for(let match of parseData) {
+  for (let match of parseData) {
     cookie[match[1]] = match[2];
   }
   return cookie;
@@ -51,59 +48,56 @@ function parseCookie(data)
 
 /*
  * Takes the default cookie sent by Socket IO and checks if the socket ID sent
- * in the cookie is already in a room in rooms. If yes, then the socket's new 
+ * in the cookie is already in a room in rooms. If yes, then the socket's new
  * ID (which is created on the new connection) replaces the ID in the room.
- * 
+ *
  * @param {Socket object} socket The socket to call the middleware on
- * @param {function}      next   A callback function 
+ * @param {function}      next   A callback function
  */
 const stickySessionMiddleware = (socket, next) => {
   const cookie = socket.request.headers.cookie;
-  if(cookie == "") {
+  if (cookie == "") {
     next();
     return;
   }
 
   const id = parseCookie(cookie)['io'];
-  if(id == undefined) {
+  if (id == undefined) {
     next();
     return;
   }
 
-  for(let roomKey of Object.keys(rooms)) {
+  for (let roomKey of Object.keys(rooms)) {
 
     const connections = rooms[roomKey].connections;
-    if(connections.includes(id)) {      
+    if (connections.includes(id)) {
 
-      for(let i = 0; i < connections.length; i++) {  
-        if(connections[i] == id) {
+      for (let i = 0; i < connections.length; i++) {
+        if (connections[i] == id) {
           connections[i] = socket.id;
           socket.join(roomKey)
         }
-      } 
+      }
 
       addSocketEventHandlers(socket);
       break;
-    } 
+    }
   }
   next();
 }
 io.use(stickySessionMiddleware);
 
-/*
- * The on-connection Socket IO handler
- */
+// The on-connection Socket IO handler
 io.on('connection', (socket) => {
   socket.send(`Connected through WebSocket. ID: ${socket.id}`);
   socket.send(`Rooms: ${JSON.stringify(rooms)}`);
-  
-  socket.on('accessRoom',(roomID) => {
 
-    if(!Object.keys(rooms).includes(roomID) || 
-          rooms[roomID].connections.length >= rooms[roomID].maxConnections) {
+  socket.on('accessRoom', (roomID) => {
+
+    if (!Object.keys(rooms).includes(roomID) ||
+      rooms[roomID].connections.length >= rooms[roomID].maxConnections) {
       socket.send("Error joining room " + roomID);
-    }
-    else {
+    } else {
       leaveRooms(socket);
       socket.join(roomID);
       rooms[roomID].connections.push(socket.id);
@@ -111,22 +105,19 @@ io.on('connection', (socket) => {
       socket.send(`Rooms: ${JSON.stringify(rooms)}`);
     }
   });
-  
 
   socket.on('disconnect', (socket) => {
     console.log("disconnected")
   })
 });
 
-
 /*
- * Adds the necessary eventhandlers to a socket ( as of now, this only includes 
+ * Adds the necessary eventhandlers to a socket ( as of now, this only includes
  * eventhandlers for maintaining an connection).
  *
  * @param {Socket object} socket The socket to apply the handlers
  */
-function addSocketEventHandlers(socket) 
-{
+function addSocketEventHandlers(socket) {
   // Disconnects the client if a ping hasn't been send in pingTimeout ms
   let sessionDrop = setTimeout(dropSession, options.pingTimeout, socket);
 
@@ -143,12 +134,11 @@ function addSocketEventHandlers(socket)
  *
  * @paran {Socket object} socket The socket to drop
  */
-function dropSession(socket) 
-{
+function dropSession(socket) {
   leaveRooms(socket)
-  
-  
-  if(socket.connected) {
+
+
+  if (socket.connected) {
     socket.send(`Disconnected from Socket IO Session`);
     socket.send(`Rooms: ${JSON.stringify(rooms)}`);
     socket.disconnect();
@@ -160,13 +150,10 @@ function dropSession(socket)
  *
  * @param {Socket object} socket The socket to leave rooms
  */
-
 function leaveRooms(socket) {
-  for(let roomKey of Object.keys(rooms))
-  {
+  for (let roomKey of Object.keys(rooms)) {
     const room = rooms[roomKey];
-    if(room.connections.includes(socket.id)) 
-    {
+    if (room.connections.includes(socket.id)) {
       socket.leave(roomKey);
       room.connections = room.connections.filter((e) => e != socket.id);
     }
